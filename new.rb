a4ee79710cc17b3a6730970edc27f597867c3156
Gabriela johnson <johnsongabriela12@gmail.com>
2020-08-10 20:11:46 +0000

    Automatically backed up by Learn

---
+++ new.rb
@@ -0,0 +1,19 @@
+class Dog 
+  def bark
+    puts "Woof!"
+  end 
+  
+  def sit 
+    puts "The dog is sitting."
+  end 
+  
+end 
+
+class Person 
+  def talk 
+    puts "Hello World!"
+  end 
+  def walk 
+    puts "The person is walking"
+  end 
+end 
